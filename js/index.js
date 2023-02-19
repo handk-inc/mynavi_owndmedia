8c158b3cfe0b4da9dde85064eebd5499fa64f4ee
りか <66503381+NakamichiRika@users.noreply.github.com>
2023-02-20 03:58:28 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-const ham = $('#js-hamburger');
-const nav = $('#js-nav');
-ham.on('click', function () { //ハンバーガーメニューをクリックしたら
-  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
