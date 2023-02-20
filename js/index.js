afaa0d787dae9b9ceb91524d4adafb18728b1001
k-hayashi5254 <k-hayashi@handk-inc.co.jp>
2023-02-21 03:12:53 +0900

    divide css per components

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-$(".openbtn1").click(function () {//ボタンがクリックされたら
-  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-});
-
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-});
-
-
-$(function () {
+$(window).on('load', function() {
   $("#header").load("/components/header.html");
+  $.getScript("../js/components/menu.js", function() {
+  });
+
   $("#footer").load("/components/footer.html");
   $("#cta-section").load("/components/cta.html");
 });
