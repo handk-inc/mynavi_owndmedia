40b336627c0dbf6cb9c0a4ca2361d3658afe95f1
k-hayashi5254 <k-hayashi@handk-inc.co.jp>
2023-02-21 23:51:00 +0900

    media list

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,4 +5,19 @@ $(window).on('load', function() {
 
   $("#footer").load("../../assets/components/footer.html");
   $("#cta-section").load("../../assets/components/cta.html");
+
+
+
+  // page inner scroll
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var adjust = -180;
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+  });
 });
